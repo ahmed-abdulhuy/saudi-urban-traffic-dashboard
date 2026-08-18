ec735bef8bf9a4292c9d08c24038df8936d37b26
Ahmed Abdelhay <ahmelhuy@gmail.com>
2026-08-18 08:56:33 +0300

    fix deployment issue

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,9 +1,13 @@
 import type { NextConfig } from 'next';
 
+const isGitHubPages = process.env.GITHUB_ACTIONS === 'true';
+
 const nextConfig: NextConfig = {
   output: 'export',
 
-  basePath: '/saudi-urban-traffic-dashboard',
+  basePath: isGitHubPages
+    ? '/saudi-urban-traffic-dashboard'
+    : '',
 
   images: {
     unoptimized: true,
