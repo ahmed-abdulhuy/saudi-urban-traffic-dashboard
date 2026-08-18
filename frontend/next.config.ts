a6a31393cec6eedbcfaaab50484c15c1b2f76cc7
Ahmed Abdelhay <ahmelhuy@gmail.com>
2026-08-18 08:36:32 +0300

    fixing bug with deploying images

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,13 +3,7 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   output: 'export',
 
-  basePath: process.env.GITHUB_ACTIONS
-    ? '/saudi-urban-traffic-dashboard'
-    : '',
-
-  assetPrefix: process.env.GITHUB_ACTIONS
-    ? '/saudi-urban-traffic-dashboard/'
-    : '',
+  basePath: '/saudi-urban-traffic-dashboard',
 
   images: {
     unoptimized: true,
